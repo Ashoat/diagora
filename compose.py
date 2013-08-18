72abfc2f197ca9aad608bbd89f2d0336c0b675cc
Ashoat Tevosyan <ashoat@gmail.com>
2013-08-18 14:39:42 -0700

    Composing posts

--- compose.py
+++ compose.py
@@ -1,9 +1,27 @@
-from flask import Blueprint
-from flask_login import login_required
+from flask import Blueprint, request, render_template, redirect, url_for
+from flask_login import login_required, current_user
+from wtforms import Form, TextField, TextAreaField, validators
+from database import select
 
 compose_bp = Blueprint('compose', __name__)
 
-@compose_bp.route('/compose')
+class ComposeForm(Form):
+  title = TextField('Title', [validators.Length(min=4, max=50)])
+  body = TextAreaField('Body', [validators.Length(min=4, max=5000)])
+
+@compose_bp.route('/compose', methods=['GET', 'POST'])
 @login_required
 def compose():
-  pass
+  form = ComposeForm(request.form)
+  if request.method == 'GET' or not form.validate():
+    return render_template('compose.html', form=form)
+  rows = select(
+    'INSERT INTO posts (title, body, "user") VALUES(%s, %s, %s) RETURNING *',
+    (form.title.data, form.body.data, current_user.get_id()),
+  )
+  if not rows:
+    return render_template(
+      'error.html',
+      message="Database error :(",
+    )
+  return redirect(url_for('post.post', post=rows[0]["id"]))
